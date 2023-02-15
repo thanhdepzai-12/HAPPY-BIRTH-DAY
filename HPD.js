ea517959c8d0682a8715b8439279371e320912cc
thanhdepzai-12 <102566654+thanhdepzai-12@users.noreply.github.com>
2023-02-15 23:47:14 +0700

    HPD

---
+++ HPD.js
@@ -0,0 +1,7 @@
+const btn = document.querySelector(".btn")
+const tab = document.querySelector(".newcontainer")
+const otab = document.querySelector(".container")
+btn.addEventListener("click" , ()=>{
+tab.style.display ="flex"
+otab.style.display ="none"
+})
